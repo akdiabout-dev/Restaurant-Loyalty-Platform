53c65f030122fa91fe6debf56390f5be816470b1
akdiabout-dev <akdiabout@gmail.com>
2026-08-30 17:05:41 +0100

    adding bcrypt and completing the architecture

---
+++ src/config/env.ts
@@ -0,0 +1,7 @@
+import "dotenv/config";
+export const env = {
+  PORT: Number(process.env.PORT ?? 5000),
+  NODE_ENV: process.env.NODE_ENV ?? "development",
+  DATABASE_URL: process.env.DATABASE_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+};
